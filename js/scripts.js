21e346e59091959bc882b612a6d830eef518972d
angelo <angelo@fluxton.co.uk>
2022-10-05 16:55:13 +0100

    active content paras :)

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,30 @@
 const panels = document.querySelectorAll('.panel')
+const blurbs = document.querySelectorAll('.blurb')
+
 
 panels.forEach(panel => {
   panel.addEventListener('click', () => {
+    // get last char
+    let panelID = panel.className.slice(-1)
     removeActiveClasses()
     panel.classList.add('active')
+    let blurb = blurbs[panelID - 1]
+    let active = blurb.classList.contains('active')
+    if (!active) {
+      removeActiveBlurbs()
+      blurb.classList.add('active')
+    }
   })
 })
 
 function removeActiveClasses() {
   panels.forEach(panel => {
     panel.classList.remove('active')
+  })
+}
+
+function removeActiveBlurbs() {
+  blurbs.forEach(blurb => {
+    blurb.classList.remove('active')
   })
 }
