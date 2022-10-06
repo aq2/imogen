77c3d150494cfab2b2f69d9778378a7a48b37db1
angelo <angelo@fluxton.co.uk>
2022-10-06 18:05:52 +0100

    click for moar, and colours

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 const panels = document.querySelectorAll('.panel')
 const blurbs = document.querySelectorAll('.blurb')
-
+const insts = document.querySelectorAll('.inst')
 
 panels.forEach(panel => {
   panel.addEventListener('click', () => {
@@ -10,9 +10,14 @@ panels.forEach(panel => {
     panel.classList.add('active')
     let blurb = blurbs[panelID - 1]
     let active = blurb.classList.contains('active')
+    // remove hidden from inst
+    let inst = insts[panelID - 1]
+    addHiddenInsts()
+    inst.classList.remove('shown')
     if (!active) {
       removeActiveBlurbs()
       blurb.classList.add('active')
+      // add hidden to inst?
     }
   })
 })
@@ -26,5 +31,11 @@ function removeActiveClasses() {
 function removeActiveBlurbs() {
   blurbs.forEach(blurb => {
     blurb.classList.remove('active')
+  })
+}
+
+function addHiddenInsts() {
+  insts.forEach(inst => {
+    inst.classList.add('shown')
   })
 }
