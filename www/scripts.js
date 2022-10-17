9600ff88607ebc9ecb94773048427b86ff4176e1
angelo <angelo@fluxton.co.uk>
2022-10-17 13:53:06 +0100

    fucking upsizing issues

--- www/scripts.js
+++ www/scripts.js
@@ -1,38 +1,45 @@
 /// panels
 
-var activePanel = 1
+var oldPanel = 1
+var newPanel = 1
 const panels = document.querySelectorAll('.panel')
 const blurbs = document.querySelectorAll('.blurb')
 const mottos = document.querySelectorAll('.motto')
 
-
 panels.forEach(panel => {
   panel.addEventListener('click', (e) => {
-    let activeNow = panel.classList.contains('active')
+    let activeNow = panel.classList.contains('shown')
     
     if (!activeNow) {
-      let panelID = parseInt(panel.dataset.idx)
+      newPanel = parseInt(panel.dataset.idx)
 
-      // switchPanels(activePanel, panelID)
-      let panelOld = panels[activePanel-1]
-      panelOld.classList.remove('active')
-      let panelNew = panels[panelID - 1]
-      panelNew.classList.add('active')
-
-      let blurbOld = blurbs[activePanel - 1]
-      blurbOld.classList.remove('active')
-      let blurbNew = blurbs[panelID - 1]
-      blurbNew.classList.add('active')
-
-      let mottoOld = mottos[activePanel - 1]
-      mottoOld.classList.add('shown')
-      let mottoNew = mottos[panelID - 1]
-      mottoNew.classList.remove('shown')
-
-      activePanel = panelID 
+      switchActives(panels, oldPanel, newPanel)
+      switchActives(blurbs, oldPanel, newPanel)
+      switchActives(mottos, newPanel, oldPanel)
+      
+      oldPanel = newPanel 
     }
   })
 })
+
+function switchActives(elements, oldID, newID, status='shown') {
+  let elementOld = elements[oldID - 1]
+  elementOld.classList.remove(status)
+  let elementNew = elements[newID - 1]
+  elementNew.classList.add(status)
+  // console.log(elements.toString())
+  // console.log(elements + ' switched')
+}
+
+function removeShowns(elements) {
+  elements.forEach(element => {
+    console.log('was ' + element.classList)
+    element.classList.remove('shown')
+    console.log('now ' + element.classList)
+
+  })
+  console.log('removed')
+}
 
 
 //end panels
@@ -62,6 +69,33 @@ if (window.innerWidth < 960) {
 onresize = () => {
   if (window.innerWidth < 960) {
     activateAll()
+    // console.log('under')
+  } else {
+    // window has been resized up!
+    // should now restore status quo
+      // restore all showns
+        // remove all shown panels and blurbs
+        // show current panel and blurb
+        // un-show current motto
+        // can o' worms for freaky corner case 🥫
+
+    // console.log('over')
+    removeShowns(panels)
+    removeShowns(blurbs)
+    // panels[newPanel - 1].classList.add('shown')
+    // console.log('old ' + oldPanel + ', new ' + newPanel)
+    switchActives(panels, newPanel, newPanel)
+    switchActives(blurbs, newPanel, newPanel)
+    
+
+    removeShowns(mottos)
+    // show mottos for inactive panels - sigh
+    // must be a better way of doing this!
+    // the content either shows a blurb or a motto!
+    switchActives(mottos, oldPanel, oldPanel)
+  
+  
+  
   }
   // else restore status quo
   // deactivate all, reactive current activeNow
@@ -69,10 +103,10 @@ onresize = () => {
 
 function activateAll() {
   panels.forEach(panel => {
-    panel.classList.add('active')
+    panel.classList.add('shown')
   })
   blurbs.forEach(blurb => {
-    blurb.classList.add('active')
+    blurb.classList.add('shown')
   })
 }
 
